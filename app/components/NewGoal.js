d8483e369f556e17c452ccd57552f4416311372a
Paul Day <paul.day.m@gmail.com>
2016-07-26 12:01:31 -0500

    user widget test

--- app/components/NewGoal.js
+++ app/components/NewGoal.js
@@ -7,6 +7,7 @@ import Footer from './Footer';
 import GoalsWidget from './sidebar/GoalsWidget';
 import LiftingForm from './subcomponents/LiftingForm';
 import CardioForm from './subcomponents/CardioForm';
+import UserWidget from './sidebar/UserWidget.js';
 
 import moment from 'moment';
 import DatePicker from 'react-datepicker';
@@ -130,6 +131,8 @@ export default class NewGoal extends React.Component {
                   </div>
 
                   <div className="col-md-4" id="side-bar">
+
+                    <UserWidget />
 
                       <button type="button" className="btn btn-success"><i className="fa fa-tachometer" aria-hidden="true"></i> View Dashboard</button>
 
--- app/components/sidebar/UserWidget.js
+++ app/components/sidebar/UserWidget.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import '../../scss/primary.scss';
 
-export default class UserSidebar extends React.Component {
+export default class UserWidget extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -10,6 +10,10 @@ export default class UserSidebar extends React.Component {
     return (
 
       <div className="panel panel-default" id="user-widget">
+
+        <h1>Hello from user sidebar</h1>
+
+      </div>
 
     );
   }
