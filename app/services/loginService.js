97aab047b6f508079caee20b8f3a9bdf683eb6d2
hermant2 <treyherman1@gmail.com>
2016-07-30 18:44:00 -0500

    forms slightly adjusted last night

--- app/services/loginService.js
+++ app/services/loginService.js
@@ -4,7 +4,7 @@ export function getAuth(resolve, reject){
   request.get("/api/auth/facebook/isauthed")
     .end((err, response)=> {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
         return resolve(response);
     })
