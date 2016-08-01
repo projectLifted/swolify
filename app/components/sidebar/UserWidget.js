ea02aca26bde15e9a1af65354d5bbb3fa585b67d
Paul Day <paul.day.m@gmail.com>
2016-07-31 19:56:34 -0500

    Pulled in data for user widget. Still need to work on schema changes, works fine with my modified profile

--- app/components/sidebar/UserWidget.js
+++ app/components/sidebar/UserWidget.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link, browserHistory} from "react-router";
+import moment from 'moment';
+
 
 import '../../scss/primary.scss';
 // import profilePic from  'https://www.filestackapi.com/api/file/E6Yx55BT2G1NmiuH34rQ';
@@ -10,28 +12,37 @@ export default class UserSidebar extends React.Component {
 
   }
   render() {
+
+    let currentDate = moment().diff(this.props.user.birthDate, 'years');
+    let editLink = `/edit-profile/${this.props.user._id}`;
+    let workoutsLink = `/workouts/${this.props.user._id}`;
+    let profileImg = {
+            backgroundImage: `url("${this.props.user.profilePicture}")`
+    };
+
+
     return (
 
       <div className="panel panel-default" id="user-widget">
 
         <div className="panel-heading"><center>
-          <i className="fa fa-user" aria-hidden="true"></i> Paul Day</center>
+          <i className="fa fa-user" aria-hidden="true"></i> {this.props.user.firstName} {this.props.user.lastName}</center>
         </div>
 
-        <div className="profile-img"></div>
+        <div style={profileImg} className="profile-img"></div>
         <div className="user-widget-content">
-          <p>Age: 33 Years Old</p>
-          <p>Weight: 155 lbs</p>
-          <p>Height: 5 ft 9 in</p>
+          <p>Age: {currentDate}</p>
+          <p>Weight: {this.props.user.startWeight} lbs</p>
+          <p>Height: {this.props.user.heightFeet} ft {this.props.user.heightInches} in</p>
           <p>Body Type: Mesomorph</p>
-          <p>Location: Dallas, Texas</p>
+          <p>Location: {this.props.user.location}</p>
           <p>Last Login: 3 Weeks Ago</p>
 
         </div>
       <div className="widget-footer">
 
-        <Link to="#"><i className="fa fa-pencil-square-o" aria-hidden="true"></i> Edit Profile</Link>
-        <Link to="#"><i className="fa fa-search" aria-hidden="true"></i> My Workouts</Link>
+        <Link to={editLink}><i className="fa fa-pencil-square-o" aria-hidden="true"></i> Edit Profile</Link>
+        <Link to={workoutsLink}><i className="fa fa-search" aria-hidden="true"></i> My Workouts</Link>
       </div>
       </div>
     );
